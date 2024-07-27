ab854678c1ab1ffd47b6f5153e5f6e34a51cd71e
Arler <arler2w@gmail.com>
2024-07-27 19:49:50 +0300

    Зарегистрированы модели board и accounts в админ панель

--- kanbanBoard/board/admin.py
+++ kanbanBoard/board/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Board, Task, Column
 
 # Register your models here.
+admin.site.register(Board)
+admin.site.register(Task)
+admin.site.register(Column)
