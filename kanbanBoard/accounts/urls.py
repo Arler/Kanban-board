fb3a8e171fafe4bcc33065c28b87012fad4fe842
Arler <arler2w@gmail.com>
2024-08-30 20:13:31 +0300

    Подкорректирован путь до API профиля.

--- kanbanBoard/accounts/urls.py
+++ kanbanBoard/accounts/urls.py
@@ -5,5 +5,5 @@ from .views import registration, profile, profile_api
 urlpatterns = [
     path("registration/", registration, name="registration"),
     path('profile/', profile, name="profile_page"),
-    path('api/profile', profile_api, name='profile_api')
+    path('api/profile/', profile_api, name='profile_api')
 ]
