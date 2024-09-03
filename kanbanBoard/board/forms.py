bc9834add9c767c92ad596a5f3e7d4ac0e432d5f
Arler <arler2w@gmail.com>
2024-09-03 09:01:20 +0300

    Удалён лишний закоментированный код.

--- kanbanBoard/board/forms.py
+++ kanbanBoard/board/forms.py
@@ -24,18 +24,3 @@ class ColumnForm(forms.ModelForm):
     class Meta:
         model = Column
         fields = '__all__'
-
-
-# class BoardEditForm(forms.Form):
-#     max_tasks = forms.IntegerField()
-
-
-# class TaskEditForm(forms.ModelForm):
-#     class Meta:
-#         model = Task
-#         fields = '__all__'
-#         exclude = ['date_created']
-
-
-# class ColumnEditForm(forms.Form):
-#     name = forms.CharField(max_length=255)
