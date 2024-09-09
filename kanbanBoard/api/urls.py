85dfab6d6a1a2a0058ecb673342890ce09bc6ac6
Arler <arler2w@gmail.com>
2024-09-09 15:44:55 +0300

    Добавлен путь для формы задачи.

--- kanbanBoard/api/urls.py
+++ kanbanBoard/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_new_board_form, get_board_settings_form
+from .views import get_new_board_form, get_board_settings_form, get_task_form
 
 urlpatterns = [
     path('forms/newboard/', get_new_board_form),
-    path('forms/boardsettings', get_board_settings_form),
+    path('forms/boardsettings/', get_board_settings_form),
+    path('forms/task/<int:pk>', get_task_form)
 ]
