0fe00de236ef9b6eb46fff83709845cd7837a018
Arler <arler2w@gmail.com>
2024-09-09 15:10:40 +0300

    Добавлена отправка формы задачи.

--- kanbanBoard/api/views.py
+++ kanbanBoard/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 
+from board.models import Task
+from board.forms import TaskForm
+
 import os
 
 # Create your views here.
@@ -14,3 +17,12 @@ def get_board_settings_form(request):
     html_file = open(os.path.join(settings.TEMPLATES[0]['DIRS'][0], 'board', 'board_settings_form.html'), 'r')
     
     return HttpResponse(html_file.read(), content_type='text/html')
+
+def get_task_form(request, pk):
+    task = Task.objects.get(pk=pk)
+    context = {
+        "form": TaskForm(),
+        "pk": task.pk,
+    }
+    template = render(request, template_name='board/task_form.html', context=context)
+    return template
