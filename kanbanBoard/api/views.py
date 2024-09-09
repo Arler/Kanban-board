53cc6be609eb07c402c4a9ed522e535c84933fa1
Arler <arler2w@gmail.com>
2024-09-09 15:14:55 +0300

    Перестроена отправка форм, оптимизированы импорты.

--- kanbanBoard/api/views.py
+++ kanbanBoard/api/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
 
 from board.models import Task
 from board.forms import TaskForm
 
-import os
-
 # Create your views here.
-def get_new_board_form(request):
-    html_file = open(os.path.join(settings.TEMPLATES[0]['DIRS'][0], 'board', 'new_board_form.html'), 'r')
-    
-    return HttpResponse(html_file.read(), content_type='text/html')
+def get_new_board_form(request):    
+    return render(request, template_name="board/forms/new_board_form.html")
 
-def get_board_settings_form(request):
-    html_file = open(os.path.join(settings.TEMPLATES[0]['DIRS'][0], 'board', 'board_settings_form.html'), 'r')
-    
-    return HttpResponse(html_file.read(), content_type='text/html')
+def get_board_settings_form(request):    
+    return render(request, template_name="board/forms/board_settings_form.html")
 
 def get_task_form(request, pk):
     task = Task.objects.get(pk=pk)
