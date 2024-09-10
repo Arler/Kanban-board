7d12c4bdeee323745aaa5a305538af004fe31f30
Arler <arler2w@gmail.com>
2024-09-10 19:12:22 +0300

    Добавлено представление для формы настройки колонки. Отредактированы представления формы новой доски и настройки доски.

--- kanbanBoard/api/views.py
+++ kanbanBoard/api/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 
-from board.models import Task
-from board.forms import TaskForm
+from board.models import Task, Board
+from board.forms import TaskForm, BoardForm, ColumnForm
 
 # Create your views here.
-def get_new_board_form(request):    
-    return render(request, template_name="board/forms/new_board_form.html")
+def get_new_board_form(request, pk):   
+    context = {
+    "form": BoardForm(),
+    "pk": pk
+    }
 
-def get_board_settings_form(request):    
-    return render(request, template_name="board/forms/board_settings_form.html")
+    return render(request, template_name="board/forms/new_board_form.html", context=context)
+
+def get_board_settings_form(request, pk):
+    board = Board.objects.get(pk=pk)
+    context = {
+        "form": BoardForm(),
+        "board": board,
+    }
+
+    return render(request, template_name="board/forms/board_settings_form.html", context=context)
 
 def get_task_form(request, pk):
-    task = Task.objects.get(pk=pk)
     context = {
         "form": TaskForm(),
-        "pk": task.pk,
+        "pk": pk,
     }
-    template = render(request, template_name='board/task_form.html', context=context)
-    return template
+
+    return render(request, template_name='board/task_form.html', context=context)
+
+def get_column_settings_form(request, pk):
+    context = {
+        "form": ColumnForm(),
+        "pk": pk,
+    }
+
+    return render(request, template_name="board/forms/column_settings_form.html", context=context)
