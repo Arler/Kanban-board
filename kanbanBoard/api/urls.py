f1fce5e6e300c0616463d3147da698174dc39f85
Arler <arler2w@gmail.com>
2024-09-10 19:11:04 +0300

    Добавлен URL путь для формы настройки колонки. Отредактирован путь для формы настройки доски.

--- kanbanBoard/api/urls.py
+++ kanbanBoard/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_new_board_form, get_board_settings_form, get_task_form
+from .views import get_new_board_form, get_board_settings_form, get_task_form, get_column_settings_form
 
 urlpatterns = [
     path('forms/newboard/', get_new_board_form),
-    path('forms/boardsettings/', get_board_settings_form),
-    path('forms/task/<int:pk>', get_task_form)
+    path('forms/boardsettings/<int:pk>/', get_board_settings_form),
+    path('forms/task/<int:pk>/', get_task_form),
+    path('forms/columnsettings/<int:pk>/', get_column_settings_form),
 ]
