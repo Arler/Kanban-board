f91c564df0eb3c1e04281b7f4878bba557595cac
Arler <arler2w@gmail.com>
2024-09-11 17:20:39 +0300

    Добавлен параметр pk к запросу по пути формы новой доски.

--- kanbanBoard/api/urls.py
+++ kanbanBoard/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import get_new_board_form, get_board_settings_form, get_task_form, get_column_settings_form
 
 urlpatterns = [
-    path('forms/newboard/', get_new_board_form),
+    path('forms/newboard/<int:pk>/', get_new_board_form),
     path('forms/boardsettings/<int:pk>/', get_board_settings_form),
     path('forms/task/<int:pk>/', get_task_form),
     path('forms/columnsettings/<int:pk>/', get_column_settings_form),
