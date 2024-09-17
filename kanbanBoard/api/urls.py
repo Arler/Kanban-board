dc7c080db0981d798920e8ef1b064fc312d84104
Arler <arler2w@gmail.com>
2024-09-17 22:59:48 +0300

    Добавлен URL путь для получения элемента описания задачи.

--- kanbanBoard/api/urls.py
+++ kanbanBoard/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_new_board_form, get_board_settings_form, get_task_form, get_column_settings_form
+from .views import get_new_board_form, get_board_settings_form, get_task_form, get_column_settings_form, get_task_description
 
 urlpatterns = [
     path('forms/newboard/<int:pk>/', get_new_board_form),
     path('forms/boardsettings/<int:pk>/', get_board_settings_form),
     path('forms/task/<int:pk>/', get_task_form),
     path('forms/columnsettings/<int:pk>/', get_column_settings_form),
+    path('elements/taskdescription/<int:pk>/', get_task_description),
 ]
