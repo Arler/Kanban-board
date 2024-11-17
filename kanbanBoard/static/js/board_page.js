1c42f477d634aeed43ccdde004ae06cf0b54c43b
Arler <arler2w@gmail.com>
2024-11-17 07:58:36 +0300

    Реализовано добавление новой колонки.

--- kanbanBoard/static/js/board_page.js
+++ kanbanBoard/static/js/board_page.js
@@ -2,6 +2,8 @@ import {getCookie, hideAllForm, hideForm} from "./default.js";
 import {ref, createApp} from "./vue.esm-browser.prod.js";
 
 
+const BOARDID = document.querySelector('.board').getAttribute('id')
+
 // Функция обработки нажатия кнопок
 function buttonResponse(event) {
     if (event.target.classList.contains('board__settings')) {
@@ -16,6 +18,9 @@ function buttonResponse(event) {
     else if (event.target.classList.contains('board-settings__delete-button')) {
         delete_column(event)
     }
+    else if (event.target.classList.contains('board-settings__new-column')) {
+        show_create_column_form(event)
+    }
 }
 
 // Отслеживание события клика по кнопкам
@@ -24,7 +29,7 @@ document.addEventListener('click', buttonResponse)
 document.addEventListener("submit", sendForm)
 
 // Функция отправки форм редактирования
-function sendForm(event) {
+async function sendForm(event) {
     let form = event.target
     let formName = form.getAttribute('name')
 
@@ -43,29 +48,75 @@ function sendForm(event) {
             }
         )
         .then(response => {return response.json()})
-        .then(json => {
-            
+        .then(board => {
+            document.querySelector('.board__title').innerHTML = board.fields.title
         })
         .catch(error => {console.log(error)})
     }
     else if (formName == "column-settings") {
-        fetch(
-            `${window.location.protocol}//${window.location.host}/api/column/`,
-            {
-                method: "PUT",
-                headers: {
-                    'X-CSRFToken': getCookie('csrftoken'),
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(Object.fromEntries(new FormData(form))),
-            }
-        )
-        .then(response => {return response.json()})
-        .then(json => {
-            update_board_columns(json[0])
-        })
-        .catch(error => {console.log(error)})
+        if (form.querySelector('input[name="id"]').getAttribute('value') != '') {
+            fetch(
+                `${window.location.protocol}//${window.location.host}/api/column/`,
+                {
+                    method: "PUT",
+                    headers: {
+                        'X-CSRFToken': getCookie('csrftoken'),
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(Object.fromEntries(new FormData(form))),
+                }
+            )
+            .then(response => {return response.json()})
+            .then(json => {
+                update_board_columns(json)
+            })
+            .catch(error => {console.log(error)})
+        }
+        else {
+            const formData = Object.fromEntries(new FormData(form))
+            formData['board-id'] = BOARDID
+            const response = await fetch(
+                `${window.location.protocol}//${window.location.host}/api/column/`,
+                {
+                    method: "POST",
+                    headers: {
+                        'X-CSRFToken': getCookie('csrftoken'),
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(formData),
+                }
+            )
+            const newColumn = await response.json()
+            console.log(newColumn)
+            create_new_column(newColumn)
+        }
     }
+}
+
+// Функция создания новой колонки
+function show_create_column_form(event) {
+    const createColumnForm = document.querySelector('.column-settings')
+    setup_column_settings_form(createColumnForm, undefined, event.target, true)
+    createColumnForm.classList.toggle('active')
+}
+
+// Функция создания новой колонки
+function create_new_column(newColumn) {
+    //Добавление новой колонки на доску
+    let newColumnNode = document.querySelector('.column').cloneNode(true)
+    let tasksContainer = newColumnNode.querySelector('.column__tasks-container')
+    while (tasksContainer.children.length > 0) {
+        tasksContainer.children[0].remove()
+    }
+    newColumnNode.setAttribute('id', `column-${newColumn.pk}`)
+    newColumnNode.querySelector('.column__title').innerText = newColumn.fields.title
+    document.querySelector('.column-container').insertAdjacentElement('beforeend', newColumnNode)
+
+    //Добавление новой колонки в меню редактирования доски
+    newColumnNode = document.querySelector('.board-settings__column').cloneNode(true)
+    newColumnNode.setAttribute('value', newColumn.pk)
+    newColumnNode.innerText = newColumn.fields.title
+    document.querySelector('.board-settings__column-container').insertAdjacentElement('beforeend', newColumnNode)
 }
 
 // Функция удаления колонки
@@ -86,6 +137,8 @@ async function delete_column(event) {
     if (response.ok) {
         document.querySelector(`.column[id="column-${columnId}"]`).remove()
         document.querySelector(`.board-settings__column[value="${columnId}"]`).remove()
+        document.querySelector('.board-settings__delete-button').classList.remove('active')
+        document.querySelector('.board-settings__edit-button').classList.remove('active')
     }
 }
 
@@ -156,12 +209,22 @@ function setup_column_buttons(column, editButton, deleteButton) {
 }
 
 // Установка формы настройки колонки
-function setup_column_settings_form(columnForm, editButton) {
-    const editButtonRect = editButton.getBoundingClientRect()
-    const columnFormRect = columnForm.getBoundingClientRect()
-    const columnId = editButton.getAttribute('value')
-    columnForm.setAttribute('value', columnId)
-    columnForm.querySelector('input[name="id"]').setAttribute('value', columnId)
-    columnForm.style.top = `${editButtonRect.top + editButtonRect.height + window.scrollY}px`
-    columnForm.style.left = `${editButtonRect.left + editButtonRect.width - columnFormRect.width}px`
+function setup_column_settings_form(columnForm, editButton=null, newColumnButton=null, newColumn=true) {
+    if (newColumn) {
+        columnForm.setAttribute('value', '')
+        columnForm.querySelector('input[name="id"]').setAttribute('value', '')
+        const newColumnButtonRect = newColumnButton.getBoundingClientRect()
+        const columnFormRect = columnForm.getBoundingClientRect()
+        columnForm.style.top = `${newColumnButtonRect.bottom + window.scrollY}px`
+        columnForm.style.left = `${newColumnButtonRect.left + newColumnButtonRect.width - columnFormRect.width}px`
+    }
+    else {
+        const editButtonRect = editButton.getBoundingClientRect()
+        const columnFormRect = columnForm.getBoundingClientRect()
+        const columnId = editButton.getAttribute('value')
+        columnForm.setAttribute('value', columnId)
+        columnForm.querySelector('input[name="id"]').setAttribute('value', columnId)
+        columnForm.style.top = `${editButtonRect.top + editButtonRect.height + window.scrollY}px`
+        columnForm.style.left = `${editButtonRect.left + editButtonRect.width - columnFormRect.width}px`
+    }
 }
