1271fb16ef716e433d57d670a995e7a7bbdb14b7
Arler <arler2w@gmail.com>
2025-01-29 19:15:52 +0300

    Добавлено отображение данных о задаче при её редактировании.

--- kanbanBoard/api/urls.py
+++ kanbanBoard/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_new_board_form, get_board_settings_form, get_task_form, get_column_settings_form, get_task_description, get_board_html
+from .views import get_new_board_form, get_board_settings_form, get_task_form, get_column_settings_form, get_task_description, get_board_html, get_task_data
 
 urlpatterns = [
     path('forms/newboard/<int:pk>/', get_new_board_form),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('forms/columnsettings/<int:pk>/', get_column_settings_form),
     path('elements/taskdescription/<int:pk>/', get_task_description),
     path('elements/board/<int:pk>/', get_board_html),
+    path('task/<int:pk>/', get_task_data),
 ]
